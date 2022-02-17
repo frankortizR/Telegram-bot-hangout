2f25b6af123638337cdc2d8e14a8753bb81a6d84
Frank Ortiz <francortizm238@gmail.com>
2022-02-17 16:49:57 -0600

    setting chat id

--- Security.py
+++ Security.py
@@ -1,6 +1,7 @@
+from env import MY_CHAT_ID
 
 def CheckingRath(update, chat_id):
-    if chat_id != 1144214477:
+    if chat_id != MY_CHAT_ID:
         update.message.reply_text('No access allowed!')
         return False
     else:
